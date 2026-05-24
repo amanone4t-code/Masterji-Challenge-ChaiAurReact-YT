9e92967cbbb453057a83b7d8989dcaaea74359f1
amanone4t-code <aman.one4t@gmail.com>
2026-05-24 16:47:17 +0530

    completed: toggleTheme Project

--- toggleTheme/src/App.jsx
+++ toggleTheme/src/App.jsx
@@ -1,12 +1,38 @@
 import { useState } from 'react'
+import { Themeprovider } from './contexts/theme'
+import { useEffect } from 'react'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState("light")
 
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  useEffect(() => {
+    const ele = document.querySelector('html').classList;
+    ele.remove("light", "dark")
+    ele.add(themeMode)
+  }, [themeMode])
   return (
-    <>
-      <h1 className='bg-blue-400'>Toggle Theme</h1>
-    </>
+    <Themeprovider value={{themeMode, darkTheme, lightTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+              <ThemeBtn/>
+            </div>
+
+            <div className="w-full max-w-sm mx-auto">
+              <Card/>
+            </div>
+        </div>
+      </div>
+    </Themeprovider>
   )
 }
 
